2a701a23c1ef8b39c889bb42af664274e5fcb46e
Fiachra O Connor <oconnorfiachra@gmail.com>
2021-04-16 21:20:24 +0100

    order details fixed works

---
+++ app/src/main/java/electric/ecomm/store/firebaseviewmodel/OrderViewModelFactory.java
@@ -0,0 +1,24 @@
+package electric.ecomm.store.firebaseviewmodel;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+
+public class OrderViewModelFactory extends ViewModelProvider.NewInstanceFactory {
+    private String mOrderKey;
+
+    public OrderViewModelFactory(String mOrderKey) {
+        this.mOrderKey = mOrderKey;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(OrderViewModel.class)) {
+            return (T) new OrderViewModel(mOrderKey);
+        } else {
+            throw new IllegalArgumentException("This is unknown ViewModel class :/ ");
+        }
+    }
+}
